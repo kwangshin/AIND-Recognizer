b23f2e61368cc2952d00b92bfa11d0edf38a8a3d
Oh, Kwangshin | Teddy | ECMD <kwangshin.oh@rakuten.com>
2017-05-03 21:32:51 +0900

    Implement the model selectors and recognizer.

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -77,7 +77,56 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        min_bic_score = float('inf')
+        best_hmm_model = None
+
+        for current_n_components in range(self.min_n_components, self.max_n_components + 1):
+            # Tip: The hmmlearn library may not be able to train or score all models. Implement try/except contructs as necessary to eliminate non-viable models from consideration.
+            try:
+                # Get hmm model of current component.
+                hmm_model = self.base_model(current_n_components)
+                # Get the log likelihood.
+                log_likelihood = hmm_model.score(self.X, self.lengths)
+
+                # Ge the number of data points
+                number_of_data_points = len(self.lengths)
+
+                # Compute the Bayesian information criteria: BIC = -2 * logL + p * logN
+                # Reference : https://en.wikipedia.org/wiki/Bayesian_information_criterion
+                #             http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+                #             https://rdrr.io/cran/HMMpa/man/AIC_HMM.html
+                # * logL = log likelihood.
+                # * N = the number of data points.
+                #
+                # ------ From the review -----------------------
+                # Here is some more complete info on p as it pertains here:
+                # "Free parameters" are parameters that are learned by the model (as opposed to ones that are provided to it, like features). We are working on some content to clarify this,because as you point out - how will they know exactly. It is complicated by the fact that the number is different, depending on how you set up your model.
+                # In this project, however, we are using the "diag" in the hmmlearn model and we are not specifying starting probabilities. Therefore, if we say that m = num_components and f = num_features, the free parameters are a sum of:
+                # (1) The free transition probability parameters, which is the size of the transmat matrix less one row because they add up to 1 and therefore the final row is deterministic, so m*(m-1)
+                # (2) The free starting probabilities, which is the size of startprob minus 1 because it adds to 1.0 and last one can be calculated so m-1
+                # (3) Number of means, which is m*f
+                # (4) Number of covariances which is the size of the covars matrix, which for "diag" is m*f
+                # * p = (1) + (2) + (3) + (4)
+                #     = m*(m-1) + (m-1) + m*f + m*f
+                #     = m^2 - m + m - 1 + m*f + m*f
+                #     = m^2 + 2*m*f - 1
+                # ---------------------------------------------------------------------
+                # * p = p denotes the number of independent parameters of the model..
+                #   p = m^2 + km - 1.
+                #     1) m : number of states in the Markov chain of the model.)
+                #     2) k : single numeric value representing the number of parameters of the underlying distribution of the observation process (e.g. k=2 for the normal distribution (mean and standard deviation)).
+                 # ---------------------------------------------------------------------
+
+                number_of_parameters = math.pow(current_n_components, 2) + 2*current_n_components*(number_of_data_points) - 1
+
+                bic_score = -2 * log_likelihood + number_of_parameters * math.log(number_of_data_points)
+                if bic_score < min_bic_score:
+                    min_bic_score = bic_score
+                    best_hmm_model = hmm_model
+            except:
+                pass
+
+        return best_hmm_model
 
 
 class SelectorDIC(ModelSelector):
@@ -86,14 +135,41 @@ class SelectorDIC(ModelSelector):
     Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
-    DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+    DIC = log(P(X(i))) - 1/(M-1)SUM(log(P(X(all but i))))
     '''
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        max_dic_score = float('-inf')
+        best_hmm_model = None
+
+        for current_n_components in range(self.min_n_components, self.max_n_components + 1):
+            # Tip: The hmmlearn library may not be able to train or score all models. Implement try/except contructs as necessary to eliminate non-viable models from consideration.
+            try:
+                hmm_model = self.base_model(current_n_components)
+                current_log_likelihood = hmm_model.score(self.X, self.lengths)
+                others_log_likelihood_list = []
+                for current_word in self.hwords:
+                    # We need to calculate except current word.
+                    if current_word != self.this_word:
+                        current_X, current_lengths = self.hwords[current_word]
+                        current_log_likeliyhood = hmm_model.score(current_X, current_lengths)
+                        others_log_likelihood_list.append(current_log_likeliyhood)
+                # M will be the the number of lielihood to be summed.
+                value_of_m = len(others_log_likelihood_list)
+                # Get DIC score.
+                # DIC = log(P(X(i))) - 1/(M-1)SUM(log(P(X(all but i))))
+                dic_score = current_log_likelihood - 1/value_of_m*sum(others_log_likelihood_list)
+
+                if dic_score > max_dic_score:
+                    max_dic_score = dic_score
+                    best_hmm_model = hmm_model
+            except:
+                pass
+
+        return best_hmm_model
 
 
 class SelectorCV(ModelSelector):
@@ -105,4 +181,52 @@ class SelectorCV(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection using CV
-        raise NotImplementedError
+        # If there is only 1 sequence, 
+        #     then let's select the model with value self.n_constant.
+        if len(self.sequences) < 2:
+            return self.base_model(self.n_constant)
+
+        # The default number of splits is 3.
+        number_of_splits = 3
+
+        if len(self.sequences) is 2:
+            # If the number of sequences is 2,
+            #    then use 2 as the number of splits.
+            # Otherwiese, keep the 3 as the number of splits.
+            number_of_splits = 2
+
+        max_avg_log_likelihood = float('-inf')
+        best_num_components = None
+
+        for cv_n_components in range(self.min_n_components, self.max_n_components + 1):
+            # List of log likelihood of this component.
+            log_likelihood = []
+
+            split_method = KFold(number_of_splits)
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                # Tip: The hmmlearn library may not be able to train or score all models. Implement try/except contructs as necessary to eliminate non-viable models from consideration.
+                try:
+                    # Combine sequence of training data.
+                    train_X, train_lengths = combine_sequences(cv_train_idx, self.sequences)
+                    # Get the model for training data.
+                    model = GaussianHMM(n_components=cv_n_components, covariance_type="diag", 
+                                    n_iter=1000, random_state=self.random_state, 
+                                    verbose=False).fit(train_X, train_lengths)
+                    # Combine sequence of test data.
+                    test_X, test_lengths = combine_sequences(cv_test_idx, self.sequences)
+                    # Get the log likelihood of test data using the model.
+                    cv_log_likelihood = model.score(test_X, test_lengths)
+                    # Add current log likelihood into the list.
+                    log_likelihood.append(cv_log_likelihood)
+                except:
+                    # Catching all exceptions and just continue the loop.
+                    pass
+
+            # Get the average log likelyhood.
+            cv_avg_log_likelihood = np.average(log_likelihood)
+            # If this average log likelyhood is the max value, then store for result.
+            if  cv_avg_log_likelihood > max_avg_log_likelihood:
+                max_avg_log_likelihood = cv_avg_log_likelihood
+                best_num_components = cv_n_components
+
+        return self.base_model(best_num_components)
--- my_recognizer.py
+++ my_recognizer.py
@@ -22,4 +22,37 @@ def recognize(models: dict, test_set: SinglesData):
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+
+    # Get for entire db of items as (X, lengths) tuple for use with hmmlearn library
+    test_set_all_items = test_set.get_all_Xlengths()
+    # Get a copy of the dictionary’s list of (key, value) pairs.
+    all_items_list_of_pairs = test_set_all_items.items()
+    # Loop all items.
+    for index, (X, lengths) in all_items_list_of_pairs:
+        word_likeliyhood_dictionary = dict()
+
+        best_guess_word = None
+        max_log_likelihood = float('-inf')
+
+        for trained_word, trained_model in models.items():
+            # Tip: The hmmlearn library may not be able to train or score all models. Implement try/except contructs as necessary to eliminate non-viable models from consideration.
+            try:
+                # Get the log likelihood.
+                current_log_likelihood = trained_model.score(X, lengths)
+                # Add the dictionary of (word: log likelihood) pair.
+                word_likeliyhood_dictionary[trained_word] = current_log_likelihood
+
+                if current_log_likelihood > max_log_likelihood:
+                    # If the current likelihood is max,
+                    #     then consider it as the best guess word!
+                    max_log_likelihood = current_log_likelihood
+                    best_guess_word = trained_word
+            except:
+                # If current item cannt be trained, then set minimum number.
+                word_likeliyhood_dictionary[trained_word] = float('-inf')
+        # Add current probability which is a dictionary type.
+        probabilities.append(word_likeliyhood_dictionary)
+
+        guesses.append(best_guess_word)
+
+    return probabilities, guesses
